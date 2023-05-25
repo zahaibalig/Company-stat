e0df15daed3ce594b23c4b60729626c96557f616
zahaibalig <zahaibalig@gmail.com>
2023-05-26 01:14:42 +0200

    app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import fetchRevenueData from './api/revenueApi';
+import RevenueChart from './components/RevenueChart';
 
 function App() {
+  const [revenueData, setRevenueData] = useState(null);
+
+  useEffect(() => {
+    // Fetch revenue data when the component mounts
+    fetchRevenueData()
+      .then(data => setRevenueData(data))
+      .catch(error => console.error(error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Company Revenue Statistics</h1>
+      {revenueData ? <RevenueChart data={revenueData} /> : <p>Loading data...</p>}
     </div>
   );
 }
