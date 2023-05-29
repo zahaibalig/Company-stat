8651d1025b250b4793179c29779f7921f31c5b8a
zahaibalig <zahaibalig@gmail.com>
2023-05-30 00:30:23 +0200

    test

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,25 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+  // If the message is in the input channel in either server
+  if (message.channel.id === inputChannelId1 || message.channel.id === inputChannelId2) {
+    // Get the output channel
+    const outputChannel = client.channels.cache.get(outputChannelId);
+
+    // If the output channel exists
+    if (outputChannel) {
+      // Check if it's a text-based channel
+      if (outputChannel.isText()) {
+        // Send the message content to the output channel
+        outputChannel.send(message.content)
+          .catch(console.error); // Log any errors
+      } else {
+        console.error(`Channel ${outputChannelId} is not a text-based channel`);
+      }
+    } else {
+      console.error(`Channel ${outputChannelId} does not exist`);
+    }
+  }
+});
