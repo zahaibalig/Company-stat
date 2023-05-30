1dc6001f45908f238187fe383374a574672b4a38
zahaibalig <zahaibalig@gmail.com>
2023-05-31 01:39:49 +0200

    app.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,104 @@
-import React, { useEffect, useState } from 'react';
-import fetchRevenueData from './api/revenueApi';
-import RevenueChart from './components/RevenueChart';
+// import React, { useEffect, useState } from 'react';
+// import fetchRevenueData from './api/revenueApi';
+// import RevenueChart from './components/RevenueChart';
 
-function App() {
-  const [revenueData, setRevenueData] = useState(null);
+// function App() {
+//   const [revenueData, setRevenueData] = useState(null);
+
+//   useEffect(() => {
+//     // Fetch revenue data when the component mounts
+//     fetchRevenueData()
+//       .then(data => setRevenueData(data))
+//       .catch(error => console.error(error));
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Company Revenue Statistics</h1>
+//       {revenueData ? <RevenueChart data={revenueData} /> : <p>Loading data...</p>}
+//       <div>
+//       <h1>Company Revenue Statistics</h1>
+
+//       {revenueData ? (
+//         <div>
+//           <RevenueChart data={revenueData} />
+
+//           {/* Additional Widgets */}
+//           <div>
+//             <h2>Widget 1</h2>
+//             {/* Widget 1 content */}
+//           </div>
+
+//           <div>
+//             <h2>Widget 2</h2>
+//             {/* Widget 2 content */}
+//           </div>
+
+//           <div>
+//             <h2>Widget 3</h2>
+//             {/* Widget 3 content */}
+//           </div>
+//         </div>
+//       ) : (
+//         <p>Loading data...</p>
+//       )}
+//     </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React, { useEffect, useState } from 'react';
+
+const App = () => {
+  const [dataStats, setDataStats] = useState({});
 
   useEffect(() => {
-    // Fetch revenue data when the component mounts
-    fetchRevenueData()
-      .then(data => setRevenueData(data))
-      .catch(error => console.error(error));
+    // Simulating an API call to fetch data stats
+    fetchDataStats()
+      .then((stats) => setDataStats(stats))
+      .catch((error) => console.error(error));
   }, []);
+
+  const fetchDataStats = () => {
+    // Replace this with your actual API call to fetch data stats
+    return new Promise((resolve, reject) => {
+      // Simulating a delay to mimic API call
+      setTimeout(() => {
+        const stats = {
+          totalUsers: 1000,
+          totalOrders: 500,
+          totalRevenue: 10000,
+        };
+        resolve(stats);
+      }, 1000);
+    });
+  };
 
   return (
     <div>
-      <h1>Company Revenue Statistics</h1>
-      {revenueData ? <RevenueChart data={revenueData} /> : <p>Loading data...</p>}
-      <div>
-      <h1>Company Revenue Statistics</h1>
-
-      {revenueData ? (
-        <div>
-          <RevenueChart data={revenueData} />
-
-          {/* Additional Widgets */}
-          <div>
-            <h2>Widget 1</h2>
-            {/* Widget 1 content */}
-          </div>
-
-          <div>
-            <h2>Widget 2</h2>
-            {/* Widget 2 content */}
-          </div>
-
-          <div>
-            <h2>Widget 3</h2>
-            {/* Widget 3 content */}
-          </div>
-        </div>
-      ) : (
-        <p>Loading data...</p>
-      )}
-    </div>
+      <h1>Data Stats</h1>
+      <p>Total Users: {dataStats.totalUsers}</p>
+      <p>Total Orders: {dataStats.totalOrders}</p>
+      <p>Total Revenue: ${dataStats.totalRevenue}</p>
     </div>
   );
-}
+};
 
 export default App;
