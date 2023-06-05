37c751a1d267cb051dfba284499d32bff1012e93
zahaibalig <zahaibalig@gmail.com>
2023-06-05 22:17:45 +0200

    main page

---
+++ src/components/main.js
@@ -0,0 +1,52 @@
+<!-- index.html -->
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Resume Page</title>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+  <header>
+    <h1>Resume</h1>
+  </header>
+  
+  <section class="resume">
+    <div class="container">
+      <h2>John Doe</h2>
+      <h3>Frontend Developer</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in volutpat tortor. Sed vel maximus purus.</p>
+      
+      <h4>Experience</h4>
+      <ul>
+        <li>Company A - Frontend Developer (2018 - Present)</li>
+        <li>Company B - Junior Developer (2016 - 2018)</li>
+      </ul>
+      
+      <h4>Skills</h4>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+      </ul>
+    </div>
+  </section>
+  
+  <section class="carousel">
+    <h2>Image Carousel</h2>
+    <div class="carousel-container">
+      <img src="image1.jpg" alt="Image 1">
+      <img src="image2.jpg" alt="Image 2">
+      <img src="image3.jpg" alt="Image 3">
+    </div>
+  </section>
+  
+  <section class="animation">
+    <h2>Animated Content</h2>
+    <div class="animated-content">
+      <p>This content fades in using JavaScript.</p>
+    </div>
+  </section>
+  
+  <script src="script.js"></script>
+</body>
+</html>
